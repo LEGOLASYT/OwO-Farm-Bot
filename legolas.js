c02f28971013a3111b83708e3d4283927b66c6c0
LEGOLASYT <88878524+LEGOLASYT@users.noreply.github.com>
2021-08-13 12:33:22 +0300

    Add files via upload

---
+++ legolas.js
@@ -0,0 +1,175 @@
+//------------------------------Tanımlar------------------------------
+const Discord = require ("discord.js-selfbot")
+const ayarlar = ("./legolas.json")
+const kanal = ayarlar.kanal
+//------------------------------Client List------------------------------
+const client = new Discord.Client()
+const client1 = new Discord.Client()
+const client2 = new Discord.Client()
+const client3 = new Discord.Client()
+const client4 = new Discord.Client()
+const client5 = new Discord.Client()
+const client6 = new Discord.Client()
+const client7 = new Discord.Client()
+const client8 = new Discord.Client()
+const client9 = new Discord.Client()
+const client10 = new Discord.Client()
+const client11 = new Discord.Client()
+const client12 = new Discord.Client()
+const client13 = new Discord.Client()
+const client14 = new Discord.Client()
+
+setInterval(() =>{ 
+const legolas = client.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client1.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client2.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client3.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client4.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client5.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client6.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client7.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client8.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client9.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client10.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client11.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client12.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client13.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+setInterval(() =>{ 
+const legolas = client14.channels.cache.get(kanal)
+legolas.send("owo")
+}, 11000)
+
+//------------------------------Aktiflik Mesajı------------------------------
+
+client.on('ready', () => {
+    console.log('OwO Bot Hazır 1. ')
+})
+
+client1.on('ready', () => {
+    console.log('OwO Bot Hazır 2. ')
+})
+
+client2.on('ready', () => {
+    console.log('OwO Bot Hazır 3. ')
+})
+
+client3.on('ready', () => {
+    console.log('OwO Bot Hazır 4. ')
+})
+
+client4.on('ready', () => {
+    console.log('OwO Bot Hazır 5. ')
+})
+
+client5.on('ready', () => {
+    console.log('OwO Bot Hazır 6.')
+})
+
+client6.on('ready', () => {
+    console.log('OwO Bot Hazır 7. ')
+})
+
+client7.on('ready', () => {
+    console.log('OwO Bot Hazır 8. ')
+})
+
+client8.on('ready', () => {
+    console.log('OwO Bot Hazır 9. ')
+})
+
+client9.on('ready', () => {
+    console.log('OwO Bot Hazır 10. ')
+})
+
+client10.on('ready', () => {
+    console.log('OwO Bot Hazır 11. ')
+})
+
+client11.on('ready', () => {
+    console.log('OwO Bot Hazır 12. ')
+})
+
+client12.on('ready', () => {
+    console.log('OwO Bot Hazır 13. ')
+})
+
+client13.on('ready', () => {
+    console.log('OwO Bot Hazır 14. ')
+})
+
+client14.on('ready', () => {
+    console.log('OwO Bot Hazır 15. ')
+})
+
+//------------------------------TOKEN LİST------------------------------
+
+client.login(process.env.tkn)
+client.login(process.env.tkn1)
+client.login(process.env.tkn2)
+client.login(process.env.tkn3)
+client.login(process.env.tkn4)
+client.login(process.env.tkn5)
+client.login(process.env.tkn6)
+client.login(process.env.tkn7)
+client.login(process.env.tkn8)
+client.login(process.env.tkn9)
+client.login(process.env.tkn10)
+client.login(process.env.tkn11)
+client.login(process.env.tkn12)
+client.login(process.env.tkn13)
+client.login(process.env.tkn14)
